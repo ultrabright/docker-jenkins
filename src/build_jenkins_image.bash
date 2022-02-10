279486df7433ed384acb48a131839b8fe884dbb7
FranzBranntvino <48475758+FranzBranntvino@users.noreply.github.com>
2022-02-10 09:11:47 +0100

    Update build_jenkins_image.bash
    Fix Typo in echoed status-line.

--- src/build_jenkins_image.bash
+++ src/build_jenkins_image.bash
@@ -2,7 +2,7 @@
 
 source docker_jenkins_scripts_globals.bash
 
-echo -n "Satrting special docker jenkins build. Please enter pass for jenkins key store : "
+echo -n "Starting special docker jenkins build. Please enter pass for jenkins key store : "
 # read -es psswrd
 
 psswrd=`gpg --quiet --gen-random --armor 0 24 |& tail -1`
